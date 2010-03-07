579635c789fbe2b80e437ef491c283d674419db3
Micheil Smith <micheil@brandedcode.com>
2010-03-08 00:54:39 +1100

    Futher enhancing the way I handle things when they go boom

--- lib/doco.rb
+++ lib/doco.rb
@@ -13,23 +13,19 @@ module Doco
       @app = app
       @url = options[:url].chomp("/") || "/"
       @root = options[:root] || Dir.pwd
-    
-      @notFoundPage = File.join(@root, "public", "404.html")
     end
   
     def call(env)
       path = Rack::Utils.unescape(File.expand_path(env['PATH_INFO']))
     
-      if path.index(@url) == 0 && env["REQUEST_METHOD"] == "GET"
+      result = if path.index(@url) == 0 && env["REQUEST_METHOD"] == "GET"
         route = path[@url.length..-1].split('/').reject {|i| i.empty? }
         route << "index" if route.empty?
 
-        respond 200, render(route)
-      else
-        result.first == 404 ? @app.call(env) : result
+        handle(route)
       end
-    rescue Errno::ENOENT => e
-      @app.call(env)
+      
+      result.first == 404 ? @app.call(env) : result
     end
       
     private 
@@ -43,19 +39,27 @@ module Doco
         return [status, headers, body]
       end
     
-      def render(route)
-        content = File.read(File.join(@root, "pages", route) + ".textile")
-        data    = {"title" => "untitled", "layout" => "default"}
+      def handle(route)
+        page = File.join(@root, "pages", route) + ".textile"
+        
+        if File.exists?(page)
+          content = File.read(page)
+          data    = {"title" => "untitled", "layout" => "default"}
       
-        if content =~ /^(---\s*\n.*?\n?)^(---\s*$\n?)/m
-          content = content[($1.size + $2.size)..-1]
-          data = data.merge(YAML.load($1))
+          if content =~ /^(---\s*\n.*?\n?)^(---\s*$\n?)/m
+            content = content[($1.size + $2.size)..-1]
+            data = data.merge(YAML.load($1))
+          end
+      
+          respond 200, mustache(load_layout(data["layout"]), data.merge({
+            :body => textile(content),
+            :base_url => @url
+          }))
+        else
+          [404, {}, ""]
         end
-      
-        mustache(load_layout(data["layout"]), data.merge({
-          :body => textile(content),
-          :base_url => @url
-        }))
+      rescue Errno::ENOENT => e
+        [404, {}, ""]
       end
       
       def load_layout(layout)
